b01e3cf4fb7d65c9289825a45a838bfebe19db4d
Stanislas Kokanosky <stanislas.kokanosky@gmail.com>
2024-12-08 09:12:39 +0100

    add flight time

--- app/Http/Controllers/FlightController.php
+++ app/Http/Controllers/FlightController.php
@@ -31,29 +31,13 @@ class FlightController extends Controller
      */
     public function store(Request $request)
     {
-        $validated = $request->validate([
-            'flight_number' => 'required|string',
-            'departure_date' => 'required|date',
-            'departure_airport' => 'required|string',
-            'departure_time' => 'required',
-            'arrival_airport' => 'required|string',
-            'arrival_time' => 'required',
-            'flight_time' => 'required|integer',
-            'airline' => 'required|string',
-            'aircraft_id' => 'required|exists:aircraft,id',
-            'aircraft_reg' => 'nullable|string',
-            'flight_class' => 'required|string|in:'.implode(',', FlightClass::values()),
-            'flight_seat' => 'required|string|in:'.implode(',', FlightSeat::values()),
-            'flight_seat_number' => 'nullable|string',
-            'flight_reason' => 'required|string|in:'.implode(',', FlightReason::values()),
-            'comments' => 'nullable|string',
-        ]);
-
+        $validated = $this->validateFlight($request);
         $validated['user_id'] = auth()->id();
 
         Flight::create($validated);
+        $this->updateUserFlightTime($validated['flight_time']);
 
-        return redirect()->route('flights.index')->with('success', 'Your flight has been created with success.');
+        return redirect()->route('flights.index')->with('success', 'Your flight has been successfully created.');
     }
 
     /**
@@ -70,7 +54,33 @@ class FlightController extends Controller
      */
     public function update(Request $request, Flight $flight)
     {
-        $validated = $request->validate([
+        $validated = $this->validateFlight($request);
+        $previousFlightTime = $flight->flight_time;
+
+        $flight->update($validated);
+        $this->updateUserFlightTime($validated['flight_time'] - $previousFlightTime);
+
+        return redirect()->route('flights.index')->with('success', 'Your flight has been successfully updated.');
+    }
+
+    /**
+     * Delete a flight.
+     */
+    public function destroy(Flight $flight)
+    {
+        $flightTime = $flight->flight_time;
+        $flight->delete();
+        $this->updateUserFlightTime(-$flightTime);
+
+        return redirect()->route('flights.index')->with('success', 'Your flight has been successfully deleted.');
+    }
+
+    /**
+     * Validate flight data.
+     */
+    private function validateFlight(Request $request)
+    {
+        return $request->validate([
             'flight_number' => 'required|string',
             'departure_date' => 'required|date',
             'departure_airport' => 'required|string',
@@ -81,25 +91,21 @@ class FlightController extends Controller
             'airline' => 'required|string',
             'aircraft_id' => 'required|exists:aircraft,id',
             'aircraft_reg' => 'nullable|string',
-            'flight_class' => 'required|string|in:'.implode(',', FlightClass::values()),
-            'flight_seat' => 'required|string|in:'.implode(',', FlightSeat::values()),
+            'flight_class' => 'required|string|in:' . implode(',', FlightClass::values()),
+            'flight_seat' => 'required|string|in:' . implode(',', FlightSeat::values()),
             'flight_seat_number' => 'nullable|string',
-            'flight_reason' => 'required|string|in:'.implode(',', FlightReason::values()),
+            'flight_reason' => 'required|string|in:' . implode(',', FlightReason::values()),
             'comments' => 'nullable|string',
         ]);
-
-        $flight->update($validated);
-
-        return redirect()->route('flights.index')->with('success', 'Your flight has been updated with success.');
     }
 
     /**
-     * Delete a flight.
+     * Update user's flight time.
      */
-    public function destroy(Flight $flight)
+    private function updateUserFlightTime(int $flightTimeChange): void
     {
-        $flight->delete();
-
-        return redirect()->route('flights.index')->with('success', 'Your flight has been deleted with success.');
+        $user = auth()->user();
+        $user->flight_time += $flightTimeChange;
+        $user->save();
     }
 }
